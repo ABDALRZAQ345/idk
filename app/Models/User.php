ab386232354dbaf823edd7dedfa80b6e154fce36
Ahmad Selo <ahmedselo2005@gmail.com>
2024-11-01 15:50:40 +0300

    Remove the phone_number from guarded.

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $guarded = ['id', 'phone_number'];
+    protected $guarded = ['id'];
 
     public function mosque(): BelongsTo
     {
